a6c1ef8e6d0f5df2ee4eb7e0fa1689625f464c89
Graham Rhodes <grhodes@gilt.com>
2013-11-23 19:41:40 +0000

    Fix db issues

--- app/dao/DataAccessLayer.scala
+++ app/dao/DataAccessLayer.scala
@@ -1,6 +1,7 @@
 package dao
 
 import play.api.Logger
+import scala.util.Try
 
 /**
  * The Data Access Layer contains all components and a profile
@@ -12,11 +13,10 @@ class DataAccessLayer(var database: driver.backend.Database) extends UserCompone
 
   import driver.profile.Implicit._
 
-  def create(implicit session: driver.backend.Session): Unit = {
-    (users.ddl ++ projects.ddl ++ userProjects.ddl ++ projectVersions.ddl ++ builds.ddl).create
-  }
+  def create(implicit session: driver.backend.Session): Unit =
+    Try{(users.ddl ++ projects.ddl ++ userProjects.ddl ++ projectVersions.ddl ++ builds.ddl).create}
 
-  def drop(implicit session: driver.backend.Session): Unit = {
+  def drop(implicit session: driver.backend.Session): Unit =
     (users.ddl ++ projects.ddl ++ userProjects.ddl ++ projectVersions.ddl ++ builds.ddl).drop
-  }
+
 }
