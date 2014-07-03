90a6fe40b8c633d313f974ef5175a227d891b994
Graham Rhodes <grhodes@gilt.com>
2014-07-03 18:43:54 +0100

    use stdout & sterr instead of logger before play is active

--- app/build/DefaultAmazonS3Client.scala
+++ app/build/DefaultAmazonS3Client.scala
@@ -8,7 +8,6 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.GetObjectRequest
 import com.typesafe.config.ConfigFactory
-import play.api.Logger
 
 import scala.util.Try
 
@@ -26,12 +25,12 @@ object DefaultAmazonS3Client {
   private def upload(file: File, filename: Option[String] = None): Boolean = {
     try {
       val s3Filename = filename.getOrElse(file.getName)
-      Logger.info(s"Uploading index '${file.getAbsolutePath}' -> '$s3Filename' to S3.")
+      println(s"Uploading index '${file.getAbsolutePath}' -> '$s3Filename' to S3.")
       amazonS3Client.putObject(config.getString("aws.s3.bucket"), s3Filename, file)
       true
     } catch {
       case ex: Exception =>
-        Logger.error(ex.getMessage, ex)
+        System.err.println(ex.getMessage, ex)
         false
     }
   }
@@ -41,7 +40,7 @@ object DefaultAmazonS3Client {
    */
   private def download(file: File, filename: String): Boolean = {
     try {
-      Logger.info(s"Downloading index '${file.getAbsolutePath}' from S3.")
+      println(s"Downloading index '${file.getAbsolutePath}' from S3.")
       amazonS3Client.getObject(
         new GetObjectRequest(config.getString("aws.s3.bucket"), filename),
         file
@@ -49,7 +48,7 @@ object DefaultAmazonS3Client {
       true
     } catch {
       case ex: Exception =>
-        Logger.error(ex.getMessage, ex)
+        System.err.println(ex.getMessage, ex)
         false
     }
   }
@@ -59,12 +58,12 @@ object DefaultAmazonS3Client {
    */
   private def delete(fileKeyName: String): Boolean = {
     try {
-      Logger.info(s"Deleting S3 file '$fileKeyName'.")
+      println(s"Deleting S3 file '$fileKeyName'.")
       amazonS3Client.deleteObject(config.getString("aws.s3.bucket"), fileKeyName)
       true
     } catch {
       case ex: Exception =>
-        Logger.error(ex.getMessage, ex)
+        System.err.println(ex.getMessage, ex)
         false
     }
   }
@@ -80,34 +79,34 @@ object DefaultAmazonS3Client {
       case ex: AmazonServiceException if ex.getStatusCode == 404 =>
         false
       case ex: Exception =>
-        Logger.error(ex.getMessage, ex)
+        System.err.println(ex.getMessage, ex)
         false
     }
   }
 
   private def uploadOrOverwrite(file: File, filename: Option[String] = None): Boolean = {
     if(doesFileExist(filename.getOrElse(file.getName))) {
-      Logger.info("Remote index exists, deleting...")
+      println("Remote index exists, deleting...")
       if(delete(filename.getOrElse(file.getName))) {
-        Logger.info("Deleted remote index, uploading new index...")
+        println("Deleted remote index, uploading new index...")
         val uploaded = upload(file, filename)
         if(uploaded) {
-          Logger.info("Remote index backed up.")
+          println("Remote index backed up.")
         } else {
-          Logger.error("Remote index not uploaded!")
+          System.err.println("Remote index not uploaded!")
         }
         uploaded
       } else {
-        Logger.error("Couldn't remove existing remote index!")
+        System.err.println("Couldn't remove existing remote index!")
         false
       }
     } else {
-      Logger.info("Remote index doesn't exist, uploading...")
+      println("Remote index doesn't exist, uploading...")
       val uploaded = upload(file, filename)
       if(uploaded) {
-        Logger.info("Remote index backed up.")
+        println("Remote index backed up.")
       } else {
-        Logger.error("Remote index not uploaded!")
+        System.err.println("Remote index not uploaded!")
       }
       uploaded
     }
@@ -123,13 +122,13 @@ object DefaultAmazonS3Client {
   }
 
   def tryRestoreIndex(file: File) = {
-    val tryRestoreOnStart = Try(ConfigFactory.load("application").getBoolean("index.tryrestoreonstart")).toOption.getOrElse(false)
+    val tryRestoreOnStart = Try(ConfigFactory.load("application").getBoolean("index.tryrestoreonstart")).getOrElse(false)
     val remoteIndexExists = if(tryRestoreOnStart) doesFileExist(DefaultRemoteIndexFilename) else false
-    Logger.info(s"Local Index Exists? - ${file.exists()}")
-    Logger.info(s"Restore index enabled? - $tryRestoreOnStart")
-    Logger.info(s"Remote index exists? - $remoteIndexExists")
+    println(s"Local Index Exists? - ${file.exists()}")
+    println(s"Restore index enabled? - $tryRestoreOnStart")
+    println(s"Remote index exists? - $remoteIndexExists")
     if(tryRestoreOnStart && !file.exists() && remoteIndexExists) {
-      Logger.info("Restoring index from S3.")
+      println("Restoring index from S3.")
       withTemporaryFile("giles_index_backup", ".zip") { tmpFile =>
         download(tmpFile, DefaultRemoteIndexFilename)
         extractDirectory(tmpFile, file)
